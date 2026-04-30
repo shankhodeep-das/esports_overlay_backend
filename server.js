eb587ec501c1345b3920d47bd43ad10ce34b7167
shankhodeep-das <shankhodeepdas609@gmail.com>
2026-04-30 22:31:48 +0530

    f

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import app from "./src/app.js";
 import { connectDB } from "./src/db/db.js";
 import startCleanupTask from "./src/services/cleanUpData.services.js";
+import cors from 'cors';
 
 connectDB();
+
+app.use(cors({
+    origin: 'https://fxaeproductions.vercel.app', // No trailing slash
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
--- src/app.js
+++ src/app.js
@@ -1,21 +1,9 @@
 import express from "express"
 import authRoutes from "./routes/auth.routes.js"
 import adminRoutes from "./routes/admin.routes.js"
-import cors from "cors"
+
 
 const app = express();
-
-
-const corsOptions = {
-  // Replace this with your EXACT frontend URL (no trailing slash)
-  origin: 'https://fxaeproductions.vercel.app', 
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-
-app.use(cors(corsOptions));
 
 app.use(express.json());
 
