232aa232ef284ed7f613aa4f32151ec8807ae83c
shankhodeep-das <shankhodeepdas609@gmail.com>
2026-05-01 18:40:20 +0530

    b

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 const app = express();
 const corsOptions = {
   origin: 'https://fxaeproductions.vercel.app',
+  //origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
