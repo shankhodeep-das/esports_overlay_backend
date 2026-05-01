f96f8908d21af5684c1d44c69514f51eaa1f4d42
shankhodeep-das <shankhodeepdas609@gmail.com>
2026-05-01 15:41:40 +0530

    small changes

--- src/app.js
+++ src/app.js
@@ -6,10 +6,19 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors({
-    origin: 'https://fxaeproductions.vercel.app',
-    credentials: true
-}));
+
+const corsOptions = {
+  origin: 'https://fxaeproductions.vercel.app',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+
+
+// Handle preflight requests for ALL routes
+app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
