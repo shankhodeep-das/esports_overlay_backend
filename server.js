d320058e9093d4927e55e3af15275b146b608f2e
shankhodeep-das <shankhodeepdas609@gmail.com>
2026-05-01 15:24:54 +0530

    changes

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ import cors from 'cors';
 
 connectDB();
 
-app.use(cors({
-    origin: 'https://fxaeproductions.vercel.app', // No trailing slash
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => {
--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from "express"
 import authRoutes from "./routes/auth.routes.js"
 import adminRoutes from "./routes/admin.routes.js"
+import cors from 'cors';
 
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use(express.json());
 
