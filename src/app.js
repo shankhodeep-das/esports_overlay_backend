1b5ec67fcace65c87d7bccb3d3ef5aa1d0497ac7
shankhodeep-das <shankhodeepdas609@gmail.com>
2026-05-01 15:25:48 +0530

    chanhges

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://fxaeproductions.vercel.app',
     credentials: true
 }));
 
